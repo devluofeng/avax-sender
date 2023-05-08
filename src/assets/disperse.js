96aa9461a882f3032462792b440ee40baf537360
RS Finance Labs <132212510+devluofeng@users.noreply.github.com>
2023-05-08 12:31:23 +0600

    deployed on avax

---
+++ src/assets/disperse.js
@@ -0,0 +1,4 @@
+export const DISPERSEABI = {
+    avaxMainnet: "0xa49d0f6E167cc7f601ba9ca0ECbFe437F3A16417", // mine
+    binanceTestnet: "0xa49d0f6E167cc7f601ba9ca0ECbFe437F3A16417", // mine
+};
---
+++ src/web3/connectors.js
@@ -0,0 +1,15 @@
+import { InjectedConnector } from '@web3-react/injected-connector';
+
+export const injectedConnector = new InjectedConnector({
+    supportedChainIds: [
+      1, // Mainet
+      3, // Ropsten
+      4, // Rinkeby
+      5, // Goerli
+      42, // Kovan
+      56, // BSC Mainnet
+      97, // BSC Testnet
+      43113, // Avax Testnet
+      4690,//IOTEX Testnet
+    ],
+  });
